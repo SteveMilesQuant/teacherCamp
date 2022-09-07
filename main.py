164336b0f055b6a367ce858e395d45f06932fc68
Steve Miles <steven.miles.quant@gmail.com>
2022-09-07 18:34:24 -0400

    skeleton page created

---
+++ main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI, Request, APIRouter
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+api_router = APIRouter()
+
+
+@api_router.get("/", status_code = 200)
+async def read_homepage(request: Request):
+    return templates.TemplateResponse("auth/login.html", {"request": request})
+
+
+@api_router.get("/login", status_code = 200)
+async def read_login(request: Request):
+    return templates.TemplateResponse("auth/login.html", {"request": request})
+
+
+@api_router.get("/register", status_code = 200)
+async def read_register(request: Request):
+    return templates.TemplateResponse("auth/register.html", {"request": request})
+
+
+app.include_router(api_router)
