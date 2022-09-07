c115c3d2148172fa18bc5c5204bef73202c03762
Steve Miles <steven.miles.quant@gmail.com>
2022-09-07 19:36:11 -0400

    made some progress with css

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/images", StaticFiles(directory="images"), name="images")
 templates = Jinja2Templates(directory="templates")
 api_router = APIRouter()
 
